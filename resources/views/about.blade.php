3ae2bc86164fd2b92b91df32eae6c56f3b4bdb05
olegserbat <olegserbat@yandex.ru>
2024-12-15 23:13:36 +0300

    Maket

---
+++ resources/views/about.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+@section('title', 'O блоге')
+@section('header', 'О Блоге')
+@section('content')
+    <p>Эксперименты с Laravel на Хекслете</p>
+@endsection
+
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/hellow', function () {
+    return 'hello, world!';
+});
+
+Route::get('/about', function () {
+    $param = [
+        'one'=>1,
+        'two'=>'twice',
+        'three'=>true
+    ];
+    return view('about', ['param'=>$param]);
+});
